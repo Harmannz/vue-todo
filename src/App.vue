ab51666d181f6ab42808a2f81de3a8af820d1b91
harmannz <harman.singh@hotmail.co.nz>
2020-07-08 21:23:24 +1200

    Add todo components with test data

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,48 @@
 <template>
-  <div id="app">
-  </div>
+    <div id="app">
+        <Todos v-bind:todos="todos"/>
+    </div>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-}
+import Todos from "./components/Todos";
+    export default {
+        name: 'App',
+        components: {
+            Todos
+        },
+        data() {
+            return {
+                todos: [
+                    {
+                        id: 1,
+                        title: 'Go workout',
+                        completed: false
+                    },
+                    {
+                        id: 2,
+                        title: 'Do laundry',
+                        completed: false
+                    },
+                    {
+                        id: 3,
+                        title: 'Cook food',
+                        completed: false
+                    },
+                    {
+                        id: 4,
+                        title: 'Clean up room',
+                        completed: false
+                    },
+                    {
+                        i: 5,
+                        title: 'Finish work',
+                        completed: false
+                    }
+                ],
+            }
+        },
+    }
 </script>
 
 <style>
