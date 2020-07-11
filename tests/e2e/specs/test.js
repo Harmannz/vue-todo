72ec74ee005b01ce8fb9d369948259a93fcaab9d
harmannz <harman.singh@hotmail.co.nz>
2020-07-12 00:00:38 +1200

    Add e2e tests for delete todos

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,7 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('My First Test', () => {
-  it('Create todos', () => {
+describe('Todo', () => {
+  it('Can create todos', () => {
     cy.visit('/')
     cy.contains('h2', 'My todolist')
 
@@ -22,6 +22,26 @@ describe('My First Test', () => {
         .first().contains('p', 'write_e2e_tests')
     cy.get('#app').get('ul').children()
         .first().next().contains('p','second_todo')
+
+  })
+
+  it('Can delete todos', () => {
+    cy.visit('/')
+    cy.contains('h2', 'My todolist')
+
+    cy.get('form').get('#action-add-todo').get("input")
+        .type('write_e2e_tests')
+        .should('have.value', 'write_e2e_tests')
+
+    cy.get('form').submit()
+
+    cy.get('#app').get('ul').children().should('have.length', 1)
+
+    cy.get('#app').get('ul')
+        .first().find('button').click()
+
+    cy.get('#app').get('ul').should('be.empty')
+
 
   })
 })
